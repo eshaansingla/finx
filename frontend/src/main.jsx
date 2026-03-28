5ed34cf147b523522202376c5c2ebf0732fd5035
Eshaan Singla <eshaansingla2807@gmail.com>
2026-03-28 20:02:33 +0530

    bugs

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,20 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+//CAPTURE GOOGLE TOKENS BEFORE REACT LOADS
+const params = new URLSearchParams(window.location.search)
+
+const access = params.get("access_token")
+const refresh = params.get("refresh_token")
+
+if (access) {
+  localStorage.setItem("access_token", access)
+  localStorage.setItem("refresh_token", refresh)
+
+  // clean URL (remove tokens)
+  window.history.replaceState({}, "", "/")
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
